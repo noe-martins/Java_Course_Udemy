f49d1a73d3f8cb9bd4846d0b3d96ddff2972ee0f
Noé Martins <noahmartins11@gmail.com>
2023-12-11 17:56:06 -0300

    feat: [Contravariância] GET NÃO PERMITIDO, PUT PERMITIDO

--- Curso_Java_Udemy_Secao19/src/application/ProgramGetPut.java
+++ Curso_Java_Udemy_Secao19/src/application/ProgramGetPut.java
@@ -6,20 +6,22 @@ import java.util.List;
 public class ProgramGetPut {
 
 	public static void main(String[] args) {
-		List<Integer> intList = new ArrayList<>();
-		intList.add(10);
-		intList.add(5);
+		List<Object> myObjs = new ArrayList<Object>();
+		myObjs.add("Maria");
+		myObjs.add("João");
 		
-		List<? extends Number> list = intList;
+		List<? super Number> myNums = myObjs;
 		
-		Number x = list.get(0);
-		System.out.println(x);
+		myNums.add(10);
+		myNums.add(35);
+		
+		System.out.println(myNums);
 		
 		/*
-		 * ERRO put -> ISSO OCORRE PORQUE A CLASSE Numbers PODE SER DE DIFERENTE TIPOS, ISSO OCORRE PORQUE O COMPILADOR
-		 * NÃO SABE SE SERÁ ADICIONADO UM NÚMERO COMPATÍVEL COM OUTRO DO TIPO Number
+		 * NÃO PODE ATRIBUIR PARA A VARIÁVEL x DO TIPO Number PORQUE A List myNums PODE SER DE OUTROS TIPOS ACIMA DE Number.
+		 * QUE NO CASO É, POIS É DO TIPO String O PRIMEIRO ELEMENTO.
 		 */
-		//list.add(20);
+		//Number x = myNums.get(0);
 	}
 
 }
