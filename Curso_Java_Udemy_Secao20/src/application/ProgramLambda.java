4b75937e375f4ad9933ff52e9834d181b39d17db
Noé Martins <noahmartins11@gmail.com>
2023-12-20 21:02:44 -0300

    feat: [Lambda V.03] Implementando interface com função lambda

--- Curso_Java_Udemy_Secao20/src/application/ProgramLambda.java
+++ Curso_Java_Udemy_Secao20/src/application/ProgramLambda.java
@@ -1,19 +1,14 @@
 package application;
 
 interface Rectangle{
-	Double area(double weight, double height);
+	Double area(double weight, double height); //ÚNICO MÉTODO ABSTRATO
 }
 
 public class ProgramLambda {
 
 	public static void main(String[] args) {
 		
-		Rectangle rec2 = new Rectangle() {
-			@Override
-			public Double area(double weight, double height) {
-				return weight * height;
-			}
-		};
+		Rectangle rec2 = (weight, height) -> weight * height;
 		
 		System.out.println(rec2.area(2.0, 4.0));
 	}
