fd8b75579ed58b125805e85c0170c3e368461675
Noé Martins <noahmartins11@gmail.com>
2023-12-28 11:11:54 -0300

    feat:[Recursividade V.1] Implementação sem Recursividade

---
+++ Curso_Java_Udemy_Secao20/src/service/ProductService.java
@@ -0,0 +1,18 @@
+package service;
+
+import java.util.List;
+
+import entities.Product;
+
+public class ProductService {
+	public Double filterSum(List<Product> list) {
+		double sum = 0.0;
+		
+		for (Product p : list) {
+			if (p.getName().charAt(0) == 'T') {
+				sum += p.getPrice();
+			}
+		}
+		return sum;
+	}
+}
