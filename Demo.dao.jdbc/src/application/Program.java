b34a5036a8501bf309e9eae8df226b83e6e29df9
Noé Martins <noahmartins11@gmail.com>
2024-02-13 10:48:19 -0300

    feat: Seller entity class

--- Demo.dao.jdbc/src/application/Program.java
+++ Demo.dao.jdbc/src/application/Program.java
@@ -1,13 +1,19 @@
 package application;
 
+import java.time.LocalDate;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		//TESTE DE FUNCIONAMENTO DO OBJETO
+		//TESTE DE FUNCIONAMENTO DOS OBJETOS
 		Department dp = new Department(1, "Técnico");
-		System.out.println(dp);
+		
+		Seller sl = new Seller(1, "Noé", "noe@gmail.com", LocalDate.now(), 3000.00, dp);
+		
+		System.out.println(sl);
 
 	}
 
