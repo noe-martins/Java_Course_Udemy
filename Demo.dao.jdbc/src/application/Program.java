36fa970dd005d58e7ddc5ecf8a8cb4e7d9f0f36d
Noé Martins <noahmartins11@gmail.com>
2024-02-13 10:25:13 -0300

    feat: Department entity class

---
+++ Demo.dao.jdbc/src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		//TESTE DE FUNCIONAMENTO DO OBJETO
+		Department dp = new Department(1, "Técnico");
+		System.out.println(dp);
+
+	}
+
+}
