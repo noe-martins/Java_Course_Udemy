bfed62c88f720cf024349c942666c87ad8d540c6
Noé Martins <noahmartins11@gmail.com>
2024-02-19 15:15:06 -0300

    feat: findByDepartment implementation

--- Demo.dao.jdbc/src/application/Program.java
+++ Demo.dao.jdbc/src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -10,11 +13,16 @@ public class Program {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		System.out.println("=== TESTE 1: finById ===");
+		System.out.println("=== TESTE 1: findById ===");
 		// TODA MANIPULAÇÃO COM O OBJETO "seller" É FEITO ATRAVÉS DO SEU "Dao" (PARA MANIPULAÕES DE BANCO DE DADOS)
 		Seller seller = sellerDao.findById(3);
-		
 		System.out.println(seller);
+		
+		System.out.println("=== TESTE 2: findByDepartment ===");
+		List<Seller> list = sellerDao.findByDepartment(new Department(2, null));
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
 
 	}
 
--- Demo.dao.jdbc/src/model/dao/impl/SellerDaoJDBC.java
+++ Demo.dao.jdbc/src/model/dao/impl/SellerDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -91,6 +94,54 @@ public class SellerDaoJDBC implements SellerDao {
 	public List<Seller> findAll() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*, department.Name AS DepName "
+					+ "FROM seller INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id "
+					+ "WHERE DepartmentId = ? "
+					+ "ORDER BY Name;"
+					);
+			st.setInt(1, department.getId());
+			rs = st.executeQuery();
+			
+			/*
+			 * ESTRUTURA PARA FAZER COM QUE VÁRIOS "Seller" APONTEM PARA O MESMO "DepartmentId"
+			 * FAZ COM QUE NÃO SE DUPLIQUE O OBJETO "Department". SE O OBJETO "Seller" É DO MESMO DEPARTAMENTO...
+			 * ENTÃO APONTARÁ PARA O MESMO OBJETO.
+			 */
+			
+			List<Seller> list = new ArrayList<>();
+			Map<Integer, Department> map = new HashMap<>();
+			
+			while(rs.next()) {
+				
+				//ESSE "map" ESTÁ VAZIO, LOGO NÃO VAI ACHAR ESSE "DepartmentId" E VAI RETORNA "null"
+				Department dep = map.get(rs.getInt("DepartmentId"));
+				
+				if(dep == null) {
+					//CRIA O OBJETO COM O DADO DA "rs"
+					dep = instantiateDepartment(rs);
+					//ADICIONA NO "map", POIS SE TIVER MAIS LINHA NA TABELA E TIVER O MESMO "DepartmentId" NÃO ENTRA NESSE "if"
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+				
+				Seller seller = instantiateSeller(rs, dep);
+				list.add(seller);
+			}
+			return list;
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 
 }
