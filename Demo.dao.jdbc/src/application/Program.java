82ff22e9abbc9c1a5d704add85d57111778562cf
Noé Martins <noahmartins11@gmail.com>
2024-02-19 16:41:25 -0300

    feat: findAll implementation

--- Demo.dao.jdbc/src/application/Program.java
+++ Demo.dao.jdbc/src/application/Program.java
@@ -23,6 +23,12 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("=== TESTE 3: findByDepartment ===");
+		list = sellerDao.findAll();
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
 
 	}
 
