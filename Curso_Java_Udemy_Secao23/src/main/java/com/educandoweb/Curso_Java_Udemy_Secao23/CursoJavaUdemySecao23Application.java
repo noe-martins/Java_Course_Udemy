0b8eacf0940d5abe100ee995ad3b8a4e9a82eb66
Noé Martins <noahmartins11@gmail.com>
2024-03-04 13:56:30 -0300

    feat: Created Project

---
+++ Curso_Java_Udemy_Secao23/src/main/java/com/educandoweb/Curso_Java_Udemy_Secao23/CursoJavaUdemySecao23Application.java
@@ -0,0 +1,13 @@
+package com.educandoweb.Curso_Java_Udemy_Secao23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoJavaUdemySecao23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoJavaUdemySecao23Application.class, args);
+	}
+
+}
---
+++ Curso_Java_Udemy_Secao23/src/test/java/com/educandoweb/Curso_Java_Udemy_Secao23/CursoJavaUdemySecao23ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.Curso_Java_Udemy_Secao23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoJavaUdemySecao23ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
